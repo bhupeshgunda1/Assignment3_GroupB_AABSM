a1861b86398d6bd00d5561d70a5031744e4e968c
SathishReddy876 <sathishreddy876@gmail.com>
2022-02-25 17:41:30 -0500

    Member servlet

--- src/main/java/com/abms/controller/MemberServlet.java
+++ src/main/java/com/abms/controller/MemberServlet.java
@@ -1,23 +1,34 @@
 package com.abms.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.abms.dao.MemberDaoClass_grpB;
+import com.abms.model.MemberPojo;
+
 /**
  * Servlet implementation class MemberServlet
  */
+@WebServlet("/MemberServ")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	MemberDaoClass_grpB memberdao = new MemberDaoClass_grpB();
     /**
      * Default constructor. 
      */
     public MemberServlet() {
         // TODO Auto-generated constructor stub
     }
+    private MemberDaoClass_grpB mdao;
+
+	public void init() {
+		mdao = new MemberDaoClass_grpB();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -32,7 +43,28 @@ public class MemberServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String memberId = request.getParameter("id");
+		String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String type = request.getParameter("type");
+        String memberdate =request.getParameter("memberdate");
+        String expirydate =request.getParameter("expirydate");
+
+        MemberPojo mm = new MemberPojo();
+        
+        mm.setMemb_id(memberId);
+        mm.setMemb_name(name);
+        mm.setAddress(address);
+        mm.setMemb_type(type);
+        mm.setMemb_date(memberdate);
+        mm.setExpiry_date(expirydate);
+        try {
+            mdao.membereg(mm);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("MemberResponse.jsp"); //response goes to view!!
 	}
 
 }
