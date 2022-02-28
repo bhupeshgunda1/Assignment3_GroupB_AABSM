70b28e4bb4ba5bee1f88c15c9523a6446a8fece0
SathishReddy876 <sathishreddy876@gmail.com>
2022-02-28 13:47:54 -0500

    Publisher Pojo class

--- src/main/java/com/abms/model/PublisherPojo.java
+++ src/main/java/com/abms/model/PublisherPojo.java
@@ -1,5 +1,30 @@
 package com.abms.model;
 
 public class PublisherPojo {
+	private String Pub_id; 
+	private String Address;
+	private String Pub_Name;
+	
+	public String getPub_id() {
+		return Pub_id;
+	}
+	public void setPub_id(String pub_id) {
+		Pub_id = pub_id;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getPub_Name() {
+		return Pub_Name;
+	}
+	public void setPub_Name(String pub_Name) {
+		Pub_Name = pub_Name;
+	}
+	
+	
+	
 
 }
