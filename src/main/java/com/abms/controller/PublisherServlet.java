a1aede666274557e10060d948f14b807142a7de4
SathishReddy876 <sathishreddy876@gmail.com>
2022-02-28 13:46:55 -0500

    Publisher Servlet

--- src/main/java/com/abms/controller/PublisherServlet.java
+++ src/main/java/com/abms/controller/PublisherServlet.java
@@ -2,15 +2,31 @@ package com.abms.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.abms.dao.MemberDaoClass_grpB;
+import com.abms.dao.PublisherDaoClass_grpB;
+import com.abms.model.MemberPojo;
+import com.abms.model.PublisherPojo;
+
 /**
  * Servlet implementation class PublisherServlet
  */
+@WebServlet("/PublisherServ")
 public class PublisherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	PublisherDaoClass_grpB publisherdao = new PublisherDaoClass_grpB();
+    /**
+     * Default constructor. 
+     */
+    private PublisherDaoClass_grpB pdao;
+
+	public void init() {
+		pdao = new PublisherDaoClass_grpB();
+	}
 
     /**
      * Default constructor. 
@@ -32,7 +48,22 @@ public class PublisherServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String pubId = request.getParameter("pub_id");
+		String address = request.getParameter("pub_address");
+		String name = request.getParameter("pub_name");
+
+        PublisherPojo pp = new PublisherPojo();
+        
+        pp.setPub_id(pubId);
+        pp.setAddress(address);
+        pp.setPub_Name(name);
+        try {
+            pdao.pubreg(pp);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("Publisher.jsp"); //response goes to view!!
 	}
 
 }
