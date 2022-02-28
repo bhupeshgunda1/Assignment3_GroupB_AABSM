023b532315e662f3673ac988c0d9785ff8860c15
SathishReddy876 <sathishreddy876@gmail.com>
2022-02-28 13:47:33 -0500

    Publisher Dao Class

--- src/main/java/com/abms/dao/PublisherDaoClass_grpB.java
+++ src/main/java/com/abms/dao/PublisherDaoClass_grpB.java
@@ -1,5 +1,33 @@
 package com.abms.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.abms.model.PublisherPojo;
+
 public class PublisherDaoClass_grpB {
+	public int pubreg(PublisherPojo pp) throws ClassNotFoundException, SQLException {
+		{
+			String INSERT_PUBLISHER_SQL ="INSERT INTO Publisher" +
+					"(Pub_id, Address, Pub_Name) VALUES " +
+					"(?,?,?);";
+			int result = 0;
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/week5", "root", "sathish@1234");
+			PreparedStatement ps = connection.prepareStatement(INSERT_PUBLISHER_SQL))
+			{
+				ps.setString(1, pp.getPub_id());
+				ps.setString(2, pp.getAddress());
+				ps.setString(3, pp.getPub_Name());
+				result = ps.executeUpdate();
+			}
+			
+			return result;
+			
+		}		
+			
+	}
 
 }
